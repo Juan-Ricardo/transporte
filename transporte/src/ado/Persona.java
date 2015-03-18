1c6a81cf7e8be3d1452ae6edb35f5bef09f25c37
Ricardo_ <Ricardo_@Ricardo>
2015-03-18 12:29:08 -0500

    Se creo la clase Persona.

---
+++ transporte/src/ado/Persona.java
@@ -0,0 +1,9 @@
+package ado;
+
+public class Persona {
+
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ transporte/src/bean/PersonaBean.java
@@ -0,0 +1,9 @@
+package bean;
+
+public class PersonaBean {
+
+	public PersonaBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
